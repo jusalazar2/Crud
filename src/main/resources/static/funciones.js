4cfb69b7be9b22da6d36c3a0c1453e1138895b61
Juan-Salazar-Saenz <42558789+Juan-Salazar-Saenz@users.noreply.github.com>
2023-10-17 01:35:01 -0500

    crud con mysql

---
+++ src/main/resources/static/funciones.js
@@ -0,0 +1,30 @@
+function eliminar(id,name,apellido) {
+	swal({
+		title: "¿ Estas seguro ?",
+		text: "¡Una vez eliminado, no podrás recuperar a "+ name +"  " +apellido+" !",
+		icon: "warning",
+		buttons: true,
+		dangerMode: true,
+	})
+		.then((OK) => {
+			if (OK) {
+				$.ajax({
+					url:"/eliminar/"+id,
+					success: function(res){
+						console.log(res);
+					}
+				})
+				swal("¡Tu registro ha sido eliminado!", {
+					icon: "success",
+				}).then((ok)=>{
+					if(ok){
+						location.href="/listar"
+					}
+				});
+			} else {
+				swal("¡Tu registro está a salvo!", {
+					icon: "success",
+				});
+			}
+		});
+}
